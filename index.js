dbda08113d3834d7eed27539a2a28681a51ada99
Veselin Dimitrov <vidimitrov94@gmail.com>
2018-01-16 08:20:22 +0200

    Add missing dependencies and rename index to server.js

--- index.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-// Main file in the NodeJS repository
-
-console.log('Welcome to the @grit nodejs-api-starter repo! Fork and good luck :)');
---
+++ server.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const http = require('http');
+const promisify = require('es6-promisify');
+const logger = require('winston');
+const Koa = require('koa');
+const config = require('./config');
+const router = require('./src/router');
+
+const app = new Koa();
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+const server = http.createServer(app.callback());
+
+const serverListen = promisify(server.listen, server);
+serverListen(config.server.port)
+  .then(() => logger.info(`App is listening on port ${config.server.port}`))
+  .catch((err) => {
+    logger.error('Error happened during server start', err);
+    process.exit(1);
+  });
+
+module.exports = server;

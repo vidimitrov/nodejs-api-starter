5522ca9b0aa18349dc293d12ee124001d7283840
Veselin Dimitrov <vidimitrov94@gmail.com>
2018-01-16 08:21:12 +0200

    Updated config to use dotenv on development

--- config/index.js
+++ config/index.js
@@ -1,4 +1,11 @@
+/* eslint-disable global-require, import/no-dynamic-require */
+
 'use strict';
+
+// Load .env in local development
+if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config({ silent: true });
+}
 
 const common = require('./components/common');
 const logger = require('./components/logger');

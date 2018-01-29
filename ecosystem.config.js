1d0a9c54a45bef4ef3aa8a5ef8c8d5763690f3fc
Veselin Dimitrov <vidimitrov94@gmail.com>
2018-01-29 14:44:49 +0200

    Add staging env config into the ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,6 +8,10 @@ module.exports = {
         PORT: 3000,
         NODE_ENV: 'development',
       },
+      env_staging: {
+        PORT: 80,
+        NODE_ENV: 'staging',
+      },
       env_production: {
         PORT: 80,
         NODE_ENV: 'production',
